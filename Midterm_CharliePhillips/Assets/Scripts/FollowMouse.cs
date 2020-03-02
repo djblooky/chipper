c1fee7322824c1b34dc6150a173fffb6a6ebc7da
Charlie Phillips <hi.charliephillips@gmail.com>
2020-03-02 15:17:31 -0600

    working on putter movement

---
+++ Midterm_CharliePhillips/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private float fixedYPosition;
+    Vector3 startpos;
+
+    void Start()
+    {
+        fixedYPosition = transform.position.y;
+        UpdateObjectPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateObjectPosition();
+    }
+
+    private void UpdateObjectPosition()
+    {
+        Debug.Log($"mouse X: {Input.mousePosition.x} mouse Y: {Input.mousePosition.y} mouse Z: {Input.mousePosition.z}");
+        Vector3 position = new Vector3(Input.mousePosition.x, fixedYPosition, Input.mousePosition.y);
+
+        transform.position = Camera.main.ScreenToWorldPoint(position) + new Vector3(10,10,12);
+    }
+}
