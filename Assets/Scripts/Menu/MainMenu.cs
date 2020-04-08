68d7d6bfe0ac0e249c217c7e020a5496aaf0a1c2
Haley Phillips <haleyphillips928@gmail.com>
2020-04-08 17:19:13 -0500

    linked main menu buttons

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject creditsMenu;
+    [SerializeField] GameObject stageSelectMenu;
+
+    public void StartGame()
     {
-        
+        //the scene needs to be registered in build settings
+        SceneManager.LoadScene("Level1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowCredits()
     {
-        
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+        stageSelectMenu.SetActive(false);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+        stageSelectMenu.SetActive(false);
+    }
+
+    public void ShowStageSelectMenu()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        stageSelectMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
