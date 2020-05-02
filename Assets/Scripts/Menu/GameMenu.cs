a860d65491122be5210e1db8cff028e4d03262cd
Haley Phillips <haleyphillips928@gmail.com>
2020-05-02 15:27:08 -0500

    end stage menu and pause menu code

--- Assets/Scripts/Menu/GameMenu.cs
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -1,22 +1,59 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject inGameUI;
+    [SerializeField] GameObject endStageMenu;
 
     public void PauseGame()
     {
         GameManager.paused = true;
         pauseMenu.SetActive(true);
         inGameUI.SetActive(false);
+        Cursor.visible = true;
     }
 
     public void UnpauseGame()
     {
-        GameManager.paused = false;
         pauseMenu.SetActive(false);
         inGameUI.SetActive(true);
+
+        Cursor.visible = false;
+        GameManager.paused = false;
+    }
+
+    public void ShowEndStageMenu()
+    {
+        inGameUI.SetActive(false);
+        pauseMenu.SetActive(false);
+        endStageMenu.SetActive(true);
+
+        Cursor.visible = true;
+        GameManager.paused = true;
+    }
+
+    public void PlayNextLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        GameManager.paused = false;
+    }
+
+    public void ToTitle()
+    {
+        SceneManager.LoadScene("MainMenu"); //load main menu
+        Cursor.visible = true;
+    }
+
+    public void ReplayLevel()
+    {
+        pauseMenu.SetActive(false);
+        inGameUI.SetActive(true);
+        endStageMenu.SetActive(false);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //reload current scene
+        GameManager.paused = false;
     }
 
 }
